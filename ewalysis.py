268d17f63a0426f681427b371cddb424086d22e3
Jason Lee <jason@jzblee.com>
2017-03-13 16:57:43 -0400

    Add peak shifting analysis, consolidate files

--- ewalysis.py
+++ ewalysis.py
@@ -4,27 +4,34 @@ import math
 import os
 from os.path import isfile, join
 
-def main():
-
-    if len( sys.argv ) == 1:
-        print ( "usage: " + sys.argv[0] + " <list of files>" )
-        print ( "       " + sys.argv[0] + " -d <directory with files>" )
-        return
-
-    files = {}  # a dictionary which uses diffraction sphere designations as keys
+files = {}  # a dictionary which uses diffraction sphere designations as keys
                 # value is currently an array of two arrays which contain
                 # data imported from the files
 
                 # format of data: intensity, x, y, z
+filelist = []
+
+def read():
+    global files
+    global filelist
+    if len( sys.argv ) == 1:
+        print ( 'usage: ' + sys.argv[0] + ' <list of files>' )
+        print ( '       ' + sys.argv[0] + ' -d <directory with files>' )
+        sys.exit(0)
+
     if len( sys.argv ) == 3 and sys.argv[1] == '-d':
         mypath = sys.argv[2]
         filelist = [f for f in os.listdir( mypath ) if isfile( join( mypath, f ) )]
     else:
+        mypath = ''
         filelist = sys.argv[1:]
-    for i in range( 0, len( filelist ) ):
+    for i in range( len( filelist ) ):
         temp = str.split( filelist[i], '/' )
         temp = temp[len( temp ) - 1]
         temp = str.split( temp, '_' )
+        if len( temp ) < 3:
+            print ( 'error: invalid file name \'' + filelist[i] + '\'' )
+            sys.exit(0)
 
         # format of temp:
         #   temp[0] - test descriptor
@@ -40,7 +47,7 @@ def main():
 
         # print ( '>>> IMPORTING ' + filekey )
 
-        for j in range( 0, 2 ):
+        for j in range( 2 ):
             filename = filelist[files[filekey][j][0]]
             # print( filename )
             file = open( join( mypath, filename ), mode='r')
@@ -74,14 +81,16 @@ def main():
                                     len( intensity_column ) == len( y_column ) and
                                     len( intensity_column ) == len( z_column ) )
             assert equal_column_length , 'Inconsistent column lengths!'
+    print('Detected ' + str( len( filelist ) ) + ' files.')
 
-    # print( files )
-
+def pba():
+    global files
+    global filelist
     for filekey in files.keys():
         center_intensity_index = 0 # storing the index so we can get the x, y, and z values
-                                   # this will be some entry in the "perfect" data
+                                   # this will be some entry in the 'perfect' data
         center_intensity = center_x = center_y = center_z = 0
-        for j in range( 0, 2 ):
+        for j in range( 2 ):
             # print( filekey + ' (' + files[filekey][j][1] + ')' )
             intensity_column = files[filekey][j][2]
             x_column = files[filekey][j][3]
@@ -90,7 +99,7 @@ def main():
             extras = files[filekey][j][6]
             # check intensities to prove differences
             intensity_sum = 0.0
-            for k in range( 0, len( intensity_column ) ):
+            for k in range( len( intensity_column ) ):
                 intensity_sum += intensity_column[k]
                 if j == 1 and intensity_column[center_intensity_index] < intensity_column[k]:
                     center_intensity_index = k
@@ -104,7 +113,7 @@ def main():
             extras.append( 0 )
             extras.append( intensity_sum )
             # print( str( center_intensity ) + ', ' + str( center_x ) + ', ' + str( center_y ) + ', ' + str( center_z ) )
-        for j in range( 0, 2 ):
+        for j in range( 2 ):
             intensity_column = files[filekey][j][2]
             x_column = files[filekey][j][3]
             y_column = files[filekey][j][4]
@@ -115,13 +124,12 @@ def main():
             center.append( center_y )
             center.append( center_z )
             max_distance = 0.0;
-            for k in range( 0, len( intensity_column ) ):
+            for k in range( len( intensity_column ) ):
                 if intensity_column[k] < 60000:
                     continue
                 distance = math.sqrt( math.pow( center_x - x_column[k], 2 ) + 
                                       math.pow( center_y - y_column[k], 2 ) +
                                       math.pow( center_z - z_column[k], 2 ) )
-                # print distance
                 if max_distance < distance:
                     max_distance = distance
             extras[0] = max_distance
@@ -145,8 +153,108 @@ def main():
         output_row += ',' + str( center[0] ) + ',' + str( center[1] ) + ',' + str( center[2] ) + '\n'
         output.write( output_row )
     output.close()
-    print('Analysed ' + str( len( filelist ) ) + ' files.')
-    print('Results saved to pba-output.csv.')
+    print('Broadening analysis results saved to pba-output.csv.')
+
+def get_distance(dist_obj):
+    return dist_obj[0]
+
+def psa():
+    global files
+    global filelist
+
+    output = open('psa-output.csv', mode='w')
+    output_row  = 'filekey,damaged_int - undamaged_int,damaged_x,damaged_y,damaged_z,undamaged_x,undamaged_y,undamaged_z\n'
+    output.write(output_row)
+
+    for filekey in files.keys():
+        # print( filekey )
+        file0 = files[filekey][0] # usually the 'damaged' test
+        file1 = files[filekey][1] # usually the 'perfect' test
+
+        f0_ic = file0[2]
+        f1_ic = file1[2]
+
+        f0_xc = file0[3]
+        f1_xc = file1[3]
+
+        f0_yc = file0[4]
+        f1_yc = file1[4]
+
+        f0_zc = file0[5]
+        f1_zc = file1[5]
+
+        f0_points = []
+        f1_points = []
+
+        for i in range( len( f0_ic ) ):
+            f0_points.append( False )
+        for i in range( len( f1_ic ) ):
+            f1_points.append( False )
+        
+        distances = []
+
+        for i in range( len( f0_ic ) ):
+            for j in range( len( f1_ic ) ):
+                distance = math.sqrt( math.pow( f1_xc[j] - f0_xc[i], 2 ) +
+                                      math.pow( f1_yc[j] - f0_yc[i], 2 ) +
+                                      math.pow( f1_zc[j] - f0_zc[i], 2 ) )
+                new_distance = [distance, i, j]
+                distances.append( new_distance )
+
+        possible_pairs = sorted( distances, key=get_distance )
+
+        # print( possible_pairs )
+
+        point_pairs = []
+
+        while possible_pairs: # while list of possible pairs is not empty
+            point_pairs.append( possible_pairs[0] )
+            f0_ind = possible_pairs[0][1]
+            f1_ind = possible_pairs[0][2]
+            f0_points[f0_ind] = True
+            f1_points[f1_ind] = True
+            del possible_pairs[0]
+            ppair_ind = 0
+            while ppair_ind < len( possible_pairs ):
+                if possible_pairs[ppair_ind][1] == f0_ind or possible_pairs[ppair_ind][1] == f1_ind:
+                    del possible_pairs[ppair_ind]
+                else:
+                    ppair_ind = ppair_ind + 1
+
+        for i in range( max( len( f0_ic ), len( f1_ic ) ) ):
+            if i < len( f0_ic ):
+                if f0_points[i]: # print point in f0 and match in f1
+                    for j in range( len( point_pairs ) ):
+                        if point_pairs[j][1] == i:
+                            break
+                    output_row  = filekey
+                    output_row += ',' + str( f0_ic[point_pairs[j][1]] - f1_ic[point_pairs[j][2]] )
+                    output_row += ',' + str( f0_xc[i] ) + ',' + str( f0_yc[i] ) + ',' + str( f0_zc[i] )
+                    output_row += ',' + str( f1_xc[i] ) + ',' + str( f1_yc[i] ) + ',' + str( f1_zc[i] ) + '\n'
+                    output.write( output_row )
+                else: # print orphan point in f0
+                    output_row  = filekey
+                    output_row += ',no match found'
+                    output_row += ',' + str( f0_xc[i] ) + ',' + str( f0_yc[i] ) + ',' + str( f0_zc[i] )
+                    output_row += ',,,\n'
+                    output.write( output_row )
+            if i < len( f1_ic ) and not f1_points[i]: # print orphan point in f1
+                output_row  = filekey
+                output_row += ',no match found'
+                output_row += ',,,'
+                output_row += ',' + str( f1_xc[i] ) + ',' + str( f1_yc[i] ) + ',' + str( f1_zc[i] ) + '\n'
+                output.write( output_row )
+
+    output.close()
+    print('Shifting analysis results saved to psa-output.csv.')
+
+def main():
+
+    read()
+    pba()
+    psa()
+
+    # print( files )
 
 if __name__== '__main__':
     main()
